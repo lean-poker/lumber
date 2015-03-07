98b7e9ca2ec29da7e2c0ef6c5432521ed83b1f53
DeVill <devillmeister@gmail.com>
2015-03-07 22:49:58 +0100

    Simplification

--- app.rb
+++ app.rb
@@ -5,23 +5,21 @@ require 'json'
 require_relative 'bootstrap'
 
 post "/api/tournament/:tournament_id/team/:repo_name/log" do
-  timestamp = t.strftime("%Y%m%d%M")
+  timestamp = Time.new.strftime("%Y%m%d%M")
   directory = "logs/#{params['tournament_id']}/#{params[:repo_name]}/"
-  filename = directory + "#{timestamp.log}"
+  filename = directory + "#{timestamp}.log"
   data = request.body.read
 
   FileUtils.mkdir_p directory
   open(filename, 'a') do |f|
     f.write data
   end
+  ''
 end
 
 get "/api/tournament/:tournament_id/team/:repo_name/log" do
-  JSON.generate Dir["logs/#{params['tournament_id']}/#{params[:repo_name]}/"]
-end
-
-get "/api/tournament/:tournament_id/team/:repo_name/log/:filename" do
-  directory = "logs/#{params['tournament_id']}/#{params[:repo_name]}/"
-  filename = directory + "#{params[:filename]}"
-  File.read(filename)
+  files = Dir["logs/#{params['tournament_id']}/#{params[:repo_name]}/*"].sort_by { |x| File.mtime(x) }
+  files.last(3).map do |filename|
+    File.read(filename)
+  end.join("\n")
 end
