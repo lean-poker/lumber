97c7072c78a4049ceef6115d7217a5d9be84c56c
Bence Kiglics <bence.kiglics@prezi.com>
2015-03-19 07:10:06 +0100

    Added file permanent store

---
+++ spec/file_permanent_store_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require_relative '../src/file_permanent_store'
+
+RSpec.describe 'FilePermanentStore' do
+  before :each do
+    @temp_dir = Dir.mktmpdir
+    @store = FilePermanentStore.new @temp_dir
+  end
+
+  after :each do
+    FileUtils.remove_entry_secure @temp_dir, true
+  end
+
+  it 'should store values' do
+    @store.store 'a', 'value'
+
+    expect(@store.retrieve 'a').to eq('value')
+  end
+
+  it 'should be able to enumerate and retrieve all existing keys' do
+    @store.store 'old-key', 'old-value'
+    @store.store 'old-key', 'another-old-value'
+
+    new_store = FilePermanentStore.new @temp_dir
+    new_store.store 'new-key', 'new-value'
+
+    expect(new_store.keys).to eq(%w(old-key new-key))
+    expect(new_store.retrieve 'old-key').to eq('another-old-value')
+  end
+end
---
+++ src/file_permanent_store.rb
@@ -0,0 +1,42 @@
+require 'digest/sha2'
+
+class FilePermanentStore
+  def initialize(dir)
+    @dir = dir
+    load_keys
+    @index_file = File.open "#{@dir}/index", 'w'
+  end
+
+  def store(key, value)
+    File.write file_name(key), value
+
+    @index_file.write "#{key}\n"
+    @index_file.flush
+    @keys[key] = 1
+  end
+
+  def retrieve(key)
+    File.read file_name(key)
+  end
+
+  def keys
+    @keys.keys
+  end
+
+  private
+
+  def file_name(key)
+    "#{@dir}/#{Digest::SHA1.hexdigest key}"
+  end
+
+  def load_keys
+    index_file_name = "#{@dir}/index"
+    @keys = Hash.new
+
+    if File.exists? index_file_name
+      File.open(index_file_name).each_line do |line|
+        @keys[line.strip] = true
+      end
+    end
+  end
+end
