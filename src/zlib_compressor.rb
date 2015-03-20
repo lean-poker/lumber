04a013cd52423a84dad157e5de046b0f8ef2c25c
Bence Kiglics <bence.kiglics@prezi.com>
2015-03-20 20:46:30 +0100

    Possibly fixed strange error from Zlib

--- src/zlib_compressor.rb
+++ src/zlib_compressor.rb
@@ -3,13 +3,20 @@ require 'zlib'
 class ZlibCompressor
   def compress(value)
     deflate = Zlib::Deflate.new
-    compressed = deflate.deflate(value, Zlib::FULL_FLUSH)
+    compressed = deflate.deflate(value, Zlib::FINISH)
     deflate.close
 
     compressed
   end
 
   def decompress(value)
-    Zlib::Inflate.new.inflate(value)
+    return '' if value.length == 0
+
+    inflate = Zlib::Inflate.new
+    decompressed = inflate.inflate(value)
+    inflate.finish
+    inflate.close
+
+    decompressed
   end
 end
